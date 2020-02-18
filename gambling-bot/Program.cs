826719d36ae39296ecc6cfc4620b2798b30d7a75
Perry MacMurray <perrymacmurray@gmail.com>
2020-02-18 16:28:02 -0500

    Add project files.

---
+++ gambling-bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gambling_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the starting point of the project. Add code here.
+        }
+    }
+}
