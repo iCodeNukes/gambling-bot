c7b373665fd257f73276d72f78737a61ee24a219
Perry MacMurray <perrymacmurray@gmail.com>
2020-02-22 18:57:50 -0500

    Created new CommandReceivedEvent, which can be subscribed to with methods meant to handle messages.

--- gambling-bot/Program.cs
+++ gambling-bot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.WebSocket;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,6 +25,8 @@ namespace gambling_bot
             await DiscordClient.LoginAsync(Discord.TokenType.Bot, File.ReadAllText("token.txt")); //token.txt stores the token to the Discord bot.
             await DiscordClient.StartAsync();
 
+            DiscordClient.MessageReceived += DiscordClient_MessageReceived;
+
             Console.WriteLine("Waiting to connect...");
             _connection.WaitOne(); //Waits for connection status to go from connecting to connected
             if (DiscordClient.ConnectionState == Discord.ConnectionState.Connected)
@@ -34,6 +37,42 @@ namespace gambling_bot
                 Environment.Exit(1);
             }
         }
+
+        public delegate void CommandReceivedEventHandler(CommandReceivedEventArgs e);
+
+        public static event CommandReceivedEventHandler CommandReceived;
+
+        public class CommandReceivedEventArgs : EventArgs
+        {
+            public string Command { set; get; }
+            public string[] CommandArgs { set; get; }
+            public ISocketMessageChannel Channel { set; get; }
+            public SocketUser User { set; get; }
+        }
+
+        private static async Task DiscordClient_MessageReceived(SocketMessage msg)
+        {
+            if (!msg.Content.StartsWith("!")) //Ignore message if it doesn't start with the command specifier "!"
+                return;
+
+            string[] splitmsg = msg.Content.Substring(1).Split(' ');
+
+            if (splitmsg.Length == 0) //If the message is just an exclamation mark don't bother
+                return;
+
+            List<string> args = new List<string>();
+            for (int i = 1; i < splitmsg.Length; i++)
+                args.Add(splitmsg[i]);
+
+            CommandReceived?.Invoke(new CommandReceivedEventArgs()
+            {
+                Command = splitmsg[0],
+                CommandArgs = args.ToArray(),
+                Channel = msg.Channel,
+                User = msg.Author
+            });
+        }
+
 
         static void Main(string[] args)
         {
@@ -49,6 +88,7 @@ namespace gambling_bot
 
             //Loading and other initialization here, while the bot connects
 
+            //Subscribe to CommandReceivedEvent here
 
             connectionTask.Wait(10000); //Wait until asynchronous execution completes, with a timeout of 10 seconds
 
